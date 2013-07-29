8f692c2c3d4c6672bd990b5acbac060e8794492b
Tom de Bruijn <tom@tomdebruijn.com>
2013-07-29 20:57:32 +0200

    Fix page layout problems
    It called the wrong layout for the new pages
    on start_new_page as it took the layout for the
    current page, rather than the page to be created.

--- lib/kotoba/export/document.rb
+++ lib/kotoba/export/document.rb
@@ -8,24 +8,34 @@ module Kotoba
       super(page_options.merge(options), &block)
     end
 
+    # Starts a new page in the Prawn document
+    #
+    # @see Prawn::Document's start_new_page method
+    #
     def start_new_page(options={})
       super(page_options.merge(options))
     end
 
+    # Returns a hash with options for page creation.
+    # The values depend on the defined layout for the to be created page.
+    #
+    # @return [Hash] hash with page options
+    #
     def page_options
+      page_layout = layout_for_next_page
       current_page_count = page_number || 1
       page_options = if current_page_count.odd?
         {
-          :left_margin => layout.margin.outer,
-          :right_margin => layout.margin.inner
+          :left_margin => page_layout.margin.outer,
+          :right_margin => page_layout.margin.inner
         }
       else
         {
-          :left_margin => layout.margin.inner,
-          :right_margin => layout.margin.outer
+          :left_margin => page_layout.margin.inner,
+          :right_margin => page_layout.margin.outer
         }
       end
-      page_options.merge(layout.to_h)
+      page_options.merge(page_layout.to_h)
     end
 
     # Adds recurring elements to all pages.
@@ -129,12 +139,46 @@ module Kotoba
       outline_chapter_headings(@headings)
     end
 
-    def config
-      Kotoba.config
+    # Returns the current page number.
+    # When no page is created yet it will return 1 instead of nil or 0
+    # (prawn default for page_number).
+    #
+    # @return [Integer] current page number
+    #
+    def current_page_number
+      return 1 if page_is_first_page?
+      page_number
     end
 
+    # Returns a boolean indicating if the current page is the first page
+    # or the first to be created page.
+    #
+    # @return [Boolean] true page is first page, false page is no first page
+    #
+    def page_is_first_page?
+      [0, nil].include?(page_number)
+    end
+
+    # Returns the layout for the current page
+    #
+    # @return [Kotoba::Layout] the layout specified for the next page
+    #
     def layout
-      config.layout_for_page([0, nil].include?(page_number) ? 1 : page_number)
+      config.layout_for_page(current_page_number)
+    end
+
+    # Returns the layout for the next page.
+    # Used for page creation in start_new_page.
+    #
+    # @return [Kotoba::Layout] the layout specified for the next page
+    #
+    def layout_for_next_page
+      next_page_number = page_is_first_page? ? 1 : (page_number + 1)
+      config.layout_for_page(next_page_number)
+    end
+
+    def config
+      Kotoba.config
     end
 
     def book
--- spec/kotoba/export/document_spec.rb
+++ spec/kotoba/export/document_spec.rb
@@ -212,6 +212,82 @@ describe Kotoba::Document do
     end
   end
 
+  describe "layout" do
+    before do
+      Kotoba.clear_config!
+      Kotoba.config do |c|
+        @first_layout = c.layout_for 1 do |l|
+          l.width = 10.cm
+          l.height = 10.cm
+        end
+        @second_layout = c.layout_for 2 do |l|
+          l.width = 20.cm
+          l.height = 20.cm
+        end
+        @default_layout = c.layout do |l|
+          l.width = 30.cm
+          l.height = 30.cm
+        end
+        @third_layout = c.layout_for 4..5 do |l|
+          l.orientation = :landscape
+          l.width = 40.cm
+          l.height = 50.cm
+        end
+      end
+    end
+
+    describe ".layout" do
+      context "with layout for specific page" do
+        it "should retrieve layout for page" do
+          # First specific layout
+          document.page_number.should == 1
+          document.layout.should == @first_layout
+          document.start_new_page
+
+          # Second specific layout
+          document.page_number.should == 2
+          document.layout.should == @second_layout
+          document.start_new_page
+
+          # Default layout
+          document.page_number.should == 3
+          document.layout.should == @default_layout
+          document.start_new_page
+
+          # Ranged layout
+          document.page_number.should == 4
+          document.layout.should == @third_layout
+        end
+      end
+    end
+
+    describe ".render" do
+      before do
+        document.text "first page"
+        document.start_new_page
+        document.text "second page"
+        document.start_new_page
+        document.text "third page"
+        document.start_new_page
+        document.text "fourth page"
+        document.start_new_page
+        document.text "fifth page"
+      end
+      subject { PDF::Inspector::Page.analyze(document.render).pages }
+
+      it "should use the configured layout per page" do
+        subject[0][:size].should == [10.cm, 10.cm]
+        subject[1][:size].should == [20.cm, 20.cm]
+        subject[2][:size].should == [30.cm, 30.cm]
+      end
+
+      it "should put page in correct orientation" do
+        subject[3][:size].should == [40.cm, 50.cm]
+        subject[4][:size].should == [40.cm, 50.cm]
+      end
+    end
+  end
+
   describe "document outline" do
     let(:headings) do
       [
